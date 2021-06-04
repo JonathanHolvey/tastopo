8514be0eba01465bdd97629c8f06752e6435bd56
Jonathan Holvey <jonathan.holvey@outlook.com>
2021-06-04 13:56:52 +1000

    Add SVG template with map rendering as PDF document

--- tastopo.py
+++ tastopo.py
@@ -16,17 +16,35 @@ Arguments:
 """
 
 import re
+from base64 import b64encode
 
 from docopt import docopt
 import requests
+from lxml import etree
+from svglib.svglib import SvgRenderer
+from reportlab.graphics import renderPDF
 
+
+SVG_NAMESPACES = {
+    'svg': 'http://www.w3.org/2000/svg',
+    'xlink': 'http://www.w3.org/1999/xlink',
+}
 
 API_URL = 'https://services.thelist.tas.gov.au/arcgis/rest/services'
 BASE_MAP = 'Topographic'
 RESOLUTION = 2000
+TEMPLATE_PATH = './templates/a4-landscape.svg'
+
+
+def svgns(fullname):
+    """Convert a SVG namespace prefix into a full namespace URI"""
+    [ns, name] = fullname.split(':')
+    namespace = SVG_NAMESPACES[ns]
+    return f'{{{namespace}}}{name}'
 
 
 def generateMap(location, scale):
+    """Generate a PDF map"""
     if not re.match(r'[-\d.]+,[-\d.]+', location):
         raise Exception('Location must be x,y coordinates')
 
@@ -39,11 +57,18 @@ def generateMap(location, scale):
         'size': f'{RESOLUTION},{RESOLUTION}',
     }
 
-    with requests.get(url, params=params, stream=True) as r:
-        r.raise_for_status()
-        with open('./map.png', 'wb') as f:
-            for chunk in r.iter_content():
-                f.write(chunk)
+    r = requests.get(url, params=params)
+    r.raise_for_status()
+    map_data = b64encode(r.content)
+    del r
+
+    template = etree.parse(TEMPLATE_PATH)
+    image_node = template.xpath('//svg:image[@id="mapData"]', namespaces=SVG_NAMESPACES)[0]
+    image_node.attrib[svgns('xlink:href')] = f'data:image/png;base64,{map_data.decode("utf-8")}'
+
+    renderer = SvgRenderer(None)
+    drawing = renderer.render(template.getroot())
+    renderPDF.drawToFile(drawing, 'map.pdf')
 
 
 if __name__ == '__main__':
