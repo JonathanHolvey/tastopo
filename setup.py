67dd6472ebd14920f5ac2f2c820b30586d2e67aa
Jonathan Holvey <jonathan.holvey@outlook.com>
2021-10-05 18:21:49 +1100

    Set version number and add changelog

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name='tastopo',
-    version='0.0.0',
+    version='1.0.0',
     description='Generate printable topographic maps for anywhere in Tasmania',
     long_description=abs_urls(long_description),
     long_description_content_type='text/markdown',
